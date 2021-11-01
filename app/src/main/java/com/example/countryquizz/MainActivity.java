4ccae282e09e23f24e5f856113d65f93fd90b915
Ikmal Azman <opikmalazman@gmail.com>
2021-11-01 16:34:59 +0800

    enable data binding

--- app/src/main/java/com/example/countryquizz/MainActivity.java
+++ app/src/main/java/com/example/countryquizz/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.countryquizz;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.*;
+
+import androidx.databinding.ViewDataBinding;
+
+import  com.example.countryquizz.R;
+import com.example.countryquizz.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
+
 }
