7ed5e8ad9d164c792e532e8257ad82f75e34872f
Ikmal Azman <opikmalazman@gmail.com>
2021-11-07 15:04:04 +0800

    add properties for data model

--- app/src/main/java/com/example/countryquizz/Quiz.java
+++ app/src/main/java/com/example/countryquizz/Quiz.java
@@ -1,2 +1,22 @@
-package com.example.countryquizz;public class Quiz {
+package com.example.countryquizz;
+
+public class Quiz {
+    private int questions;
+    private boolean answerTrue;
+    private int imageName;
+
+    public Quiz(int questions, int imageName, boolean answerTrue) {
+        this.questions = questions;
+        this.answerTrue = answerTrue;
+        this.imageName = imageName;
+    }
+
+    public int getQuestions() {
+        return questions;
+    };
+    public int getImageName() {return  imageName;};
+    public  boolean isAnswerTrue() {
+        return  answerTrue;
+    };
+
 }
